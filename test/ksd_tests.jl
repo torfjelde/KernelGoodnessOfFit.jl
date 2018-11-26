75d05be89cd57da25e1d72f101a7f153bcea4d8b
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2018-11-26 16:56:24 +0000

    made KSD test less computational intesive

--- test/ksd_tests.jl
+++ test/ksd_tests.jl
@@ -12,11 +12,11 @@ q_ = MultivariateNormal([μ, 5.0], [σ² 0; 0 σ²])  # false
 vs_ = [0.0 ; 0.0]
 vs_ = reshape(vs_, 2, 1)
 
-n_ = 400
+n_ = 100
 xs_ = rand(p_, n_)
 
 # simple example
-T = GoodnessOfFit.KSDTest(rbf_, 100, 0.05)
+T = GoodnessOfFit.KSDTest(rbf_, 10, 0.05)
 
-@test perform(T, q_, xs_)
-perform(T, p_, xs_)
+@test perform(T, q_, xs_).result == :reject
+@test perform(T, p_, xs_).result == :accept
